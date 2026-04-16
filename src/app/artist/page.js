ad3189ddc7febb88c8be8a384bad57585c72d33a
VollardBlack <concierge@vollardblack.com>
2026-04-16 14:17:53 +0200

    Update page.js

--- src/app/artist/page.js
+++ src/app/artist/page.js
@@ -1,8 +1,4 @@
 'use client';
 import dynamic from 'next/dynamic';
-
 const ArtistPortal = dynamic(() => import('../ArtistPortal'), { ssr: false });
-
-export default function ArtistPage() {
-  return <ArtistPortal />;
-}
+export default function ArtistPage() { return <ArtistPortal />; }
