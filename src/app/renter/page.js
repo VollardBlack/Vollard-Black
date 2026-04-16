e8bc5b93c5146e550da4c0102014c746f2913818
VollardBlack <concierge@vollardblack.com>
2026-04-16 21:31:00 +0200

    Update page.js

--- src/app/renter/page.js
+++ src/app/renter/page.js
@@ -1,4 +1,5 @@
 'use client';
-import dynamic from 'next/dynamic';
-const RenterPortal = dynamic(() => import('../RenterPortal'), { ssr: false });
-export default function RenterPage() { return <RenterPortal />; }
+import App from './VollardBlack';
+export default function Home() {
+  return <App />;
+}
