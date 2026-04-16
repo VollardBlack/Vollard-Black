aaae637562fa51c5580f9f9eb134ea19dcc737ac
VollardBlack <concierge@vollardblack.com>
2026-04-16 14:18:11 +0200

    Update page.js

--- src/app/buyer/page.js
+++ src/app/buyer/page.js
@@ -1,8 +1,4 @@
 'use client';
 import dynamic from 'next/dynamic';
-
 const BuyerPortal = dynamic(() => import('../BuyerPortal'), { ssr: false });
-
-export default function BuyerPage() {
-  return <BuyerPortal />;
-}
+export default function BuyerPage() { return <BuyerPortal />; }
