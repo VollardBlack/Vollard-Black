224ec5a2defdfb549bc224452d2460c5ace4bb19
VollardBlack <concierge@vollardblack.com>
2026-04-25 12:13:00 +0200

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,4 +1,4 @@
 'use client';
 import dynamic from 'next/dynamic';
-const VollardBlack = dynamic(() => import('./VollardBlack.jsx'), { ssr: false });
+const VollardBlack = dynamic(() => import('../VollardBlack.jsx'), { ssr: false });
 export default function AdminPage() { return <VollardBlack />; }
