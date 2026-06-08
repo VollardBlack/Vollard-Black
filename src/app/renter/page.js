101e96d97b67c10fd09ad52a0a0127c38df4703e
VollardBlack <concierge@vollardblack.com>
2026-06-08 11:36:37 +0200

    Delete src directory

--- src/app/renter/page.js
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const RenterPortal = dynamic(() => import('../RenterPortal'), { ssr: false });
-export default function RenterPage() { return <RenterPortal />; }
