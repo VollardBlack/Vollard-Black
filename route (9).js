0492c141fab289694be774591089971aadbf430d
VollardBlack <concierge@vollardblack.com>
2026-06-08 17:04:01 +0200

    Delete route (9).js

--- route (9).js
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const ArtistPortal = dynamic(() => import('../ArtistPortal'), { ssr: false });
-export default function ArtistPage() { return <ArtistPortal />; }
