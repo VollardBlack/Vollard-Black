c7850fbdf688e38024ed0857140015e79cd630a1
VollardBlack <concierge@vollardblack.com>
2026-06-08 11:07:35 +0200

    Delete src directory

--- src/app/buyer/page.js
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const BuyerPortal = dynamic(() => import('../BuyerPortal.jsx'), { ssr: false });
-export default function BuyerPage() { return <BuyerPortal />; }
--- src/app/page.js
+++
@@ -1,4 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const HubPage = dynamic(() => import('./HubPage.jsx'), { ssr: false });
-export default function Page() { return <HubPage />; }
