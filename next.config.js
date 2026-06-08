d84cd62d07ce7d2836394680342e206a65ccd0dc
VollardBlack <concierge@vollardblack.com>
2026-06-08 12:08:14 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,4 @@
+'use client';
+import dynamic from 'next/dynamic';
+const BuyerPortal = dynamic(() => import('../BuyerPortal'), { ssr: false });
+export default function BuyerPage() { return <BuyerPortal />; }
